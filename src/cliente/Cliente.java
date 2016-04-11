6d2befcd735bb77d4eb3a61e8de433a2644f56da
ademir <ademirbezerra2012@gmail.com>
2016-04-11 07:57:15 -0300

    cliente 1.1

--- src/cliente/Cliente.java
+++ src/cliente/Cliente.java
@@ -22,7 +22,9 @@ import java.io.ObjectOutputStream;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.net.Socket;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Scanner;
 import javax.swing.JFileChooser;
 
@@ -42,149 +44,206 @@ public class Cliente {
     private long tamanhoPermitidoKB = 5120;
     
     private static Usuario usuario;
+    private static ObjectOutputStream oos;
+    private static ObjectInputStream ois;
     
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) throws IOException, ClassNotFoundException {
+    public static void main(String[] args) throws IOException, ClassNotFoundException, InterruptedException {
         
+        boolean close = false;
         
-        System.out.println("OneBox\n\n [1] - registrar\n[2] - login");
-        Scanner s = new Scanner(System.in);
+        Socket socket = new Socket(IP, PORTA); 
         
-        int i = s.nextInt();
-        s.nextLine();
+        oos = new ObjectOutputStream(socket.getOutputStream());
+        ois = new ObjectInputStream(socket.getInputStream());
         
-        switch(i){
+        while(!close) {
             
-            case 1:
-                
-                System.out.print("Digite o login:");
-                String login1 = s.nextLine();
-                System.out.print("Digite a senha:");
-                String senha1 = s.nextLine();
-                System.out.print("Confirme a senha:");
-                String senha2 = s.nextLine();
-                
-                if(senha1.equals(senha2)){
-                    registrar(login1,senha1);
-                } else {
-                    System.out.println("Senhas erradas!");
-                }
-                
-                break;
-                
-            case 2:
-                
-                System.out.println("Digite o login");
-                String login = s.nextLine();
-                System.out.println("Digite a senha");
-                String senha = s.nextLine();
-                
-                if(logar(login, senha)){
-                    syncronaizer();
-                } else {
+            System.out.println("OneBox\n\n[1] - registrar\n[2] - login\n[3] - Sair");
+            Scanner s = new Scanner(System.in);
+
+            int i = s.nextInt();
+            s.nextLine();
+
+            switch(i){
+
+                case 1:
+
+                    System.out.print("Digite o login:");
+                    String login1 = s.nextLine();
+                    System.out.print("Digite a senha:");
+                    String senha1 = s.nextLine();
+                    System.out.print("Confirme a senha:");
+                    String senha2 = s.nextLine();
+
+                    if(senha1.equals(senha2)){
+                        registrar(login1,senha1,oos,ois);
+                    } else {
+                        System.out.println("Senhas erradas!");
+                    }
                     
-                }
-                
-                break;
-            default:
-                break;
+                    break;
+
+                case 2:
+
+                    System.out.println("Digite o login");
+                    String login = s.nextLine();
+                    System.out.println("Digite a senha");
+                    String senha = s.nextLine();
+                    
+                    if(logar(login, senha, socket, oos, ois)){
+
+                        syncronaizer(socket);
+                        
+                    } else {}
+                    
+                    break;
+
+                case 3:
+                    close = true;
+                    break;
+                    
+                default:
+                    break;
+            }
+            
+            
         }
         
+        oos.close();
+        ois.close();
+        socket.close();
         
-            FileInputStream fis;
-            try {
-            
-                JFileChooser chooser = new JFileChooser();
-                chooser.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
-                chooser.setDialogTitle("Escolha o arquivo");
-                
-                int returnVal = chooser.showOpenDialog(null);
-                
-                if (returnVal == JFileChooser.APPROVE_OPTION) {
+        
+//                    File fileSelected = chooser.getSelectedFile();
+//                    
+//                    byte[] bFile = new byte[(int) fileSelected.length()];
+//                    fis = new FileInputStream(fileSelected);
+//                    fis.read(bFile);
+//                    fis.close();
+//                    
+//                    long kbSize = fileSelected.length() / 1024;
+//                    
+//                    //request = new Request(fileSelected.getName(),bFile,"/home/ademir/OneBox/",new Date());
+//                    
+//                    //enviarArquivoServidor();
+//                    
+//                    Socket socket = new Socket(IP, PORTA); 
+//                    
+//                    ObjectOutputStream oos = new ObjectOutputStream(socket.getOutputStream());
+//                    oos.writeObject(request);
+//                    
+//                    ObjectInputStream ois = new ObjectInputStream(socket.getInputStream());
+//                    Request request = (Request) ois.readObject();
+//                    System.out.println(request.getNome());
+//                    
+//                    socket.close();
                     
-                    File fileSelected = chooser.getSelectedFile();
-                    
-                    byte[] bFile = new byte[(int) fileSelected.length()];
-                    fis = new FileInputStream(fileSelected);
-                    fis.read(bFile);
-                    fis.close();
-                    
-                    long kbSize = fileSelected.length() / 1024;
-                    
-                    //request = new Request(fileSelected.getName(),bFile,"/home/ademir/OneBox/",new Date());
-                    
-                    //enviarArquivoServidor();
-                    
-                    Socket socket = new Socket(IP, PORTA); 
-                    
-                    ObjectOutputStream oos = new ObjectOutputStream(socket.getOutputStream());
-                    oos.writeObject(request);
-                    
-                    ObjectInputStream ois = new ObjectInputStream(socket.getInputStream());
-                    Request request = (Request) ois.readObject();
-                    System.out.println(request.getNome());
-                    
-                    socket.close();
-                    
-                }
-                
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
  
     }
    
     
-    private static void registrar(String login, String senha) throws IOException, ClassNotFoundException {
-        Socket socket = new Socket(IP, PORTA); 
+    private static void registrar(String login, String senha, ObjectOutputStream oos, ObjectInputStream ois) throws IOException, ClassNotFoundException {
+       
         
         Request requestRegister = new Request(login,senha,"registrar");
         
-        ObjectOutputStream oos = new ObjectOutputStream(socket.getOutputStream());
         oos.writeObject(requestRegister);
         
-        ObjectInputStream ois = new ObjectInputStream(socket.getInputStream());
         Reply reply = (Reply) ois.readObject();
+        
         System.out.println(reply.getObs());
-
-        socket.close();
+        
     }
     
-    public static boolean logar(String login, String senha) throws IOException, ClassNotFoundException{
-        
-        Socket socket = new Socket(IP, PORTA); 
+    public static boolean logar(String login, String senha, Socket socket, ObjectOutputStream oos, ObjectInputStream ois) throws IOException, ClassNotFoundException{
         
         Request requestRegister = new Request(login,senha,"logar");
         
-        ObjectOutputStream oos = new ObjectOutputStream(socket.getOutputStream());
         oos.writeObject(requestRegister);
         
-        ObjectInputStream ois = new ObjectInputStream(socket.getInputStream());
         Reply reply = (Reply) ois.readObject();
-        System.out.println(reply.getObs());
         
         usuario = new Usuario();
         usuario.setId(reply.getUserId());
         usuario.setLogin(reply.getLogin());
         usuario.setSenha(reply.getSenha());
         
-        socket.close();
+        System.out.println(reply.getObs());
         
         if(reply.getObs().equals("Autenticado !")){
             return true;
         } else {
             return false;
-        }   
+        }
     
     }
     
-    public static void syncronaizer(){
+    public static void syncronaizer(Socket socket) throws InterruptedException, IOException, ClassNotFoundException{
+        
+        System.out.println("...Sincronizando...");
+        ListFilesUtil listFilesUtil = new ListFilesUtil();
+        List<String> paths = new ArrayList<String>();
         
         while(true){
             
+            Request request = new Request();
+
+            request.setLogin(usuario.getLogin());
+            request.setSenha(usuario.getSenha());
+            request.setOperacao("e/r lista de arquivos");
+            request.setPaths(paths);
+
+            Reply reply = sendAndReceiverObjectRequestReply(request, socket, oos, ois);
+
+            for(String key : reply.getPaths()){    
+                System.out.println(key);
+            }
+            
+//            paths = listFilesUtil.listFilesAndFilesSubDirectories(paths, dir);
+//            
+//            for( String key : paths ){
+//                System.out.println(key);
+//            }
+            
+            boolean equals = false;
+            
+//            for(String key : reply.getPaths()){    
+//                
+//                for(String key2 : paths){
+//                    
+//                    if(key.equals(key2)){
+//                        equals = true;
+//                    } else {
+//                        equals = false;
+//                    }
+//                    
+//                }
+//                
+//                if(!equals){
+//                    System.out.println(key);
+//                }
+//                
+//            }
+            
+            
+            
+            Thread.sleep(7000);
+            
         }
+    }
+    
+    public static Reply sendAndReceiverObjectRequestReply(Request request, Socket socket, ObjectOutputStream oos, ObjectInputStream ois) throws IOException, ClassNotFoundException{
+        
+        oos.writeObject(request);
+        
+        Reply reply = (Reply) ois.readObject();
+        
+        System.out.println(reply.getObs());
+        
+        return reply;
         
     }
     
